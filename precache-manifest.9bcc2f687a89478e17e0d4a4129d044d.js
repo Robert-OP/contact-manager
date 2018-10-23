02f70315c6476d45c58d318261969b4f3a8b2b63
Robert-OP <popescu.robert.octavian@gmail.com>
2018-10-23 17:42:07 +0200

    Updates

---
+++ precache-manifest.9bcc2f687a89478e17e0d4a4129d044d.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "773e1aca3230ac840ef9",
+    "url": "/Robert-OP/contact-manager/static/js/runtime~main.773e1aca.js"
+  },
+  {
+    "revision": "9866c579b5c9c66208c7",
+    "url": "/Robert-OP/contact-manager/static/js/main.9866c579.chunk.js"
+  },
+  {
+    "revision": "5e73e9b3d7a43029ad7b",
+    "url": "/Robert-OP/contact-manager/static/js/1.5e73e9b3.chunk.js"
+  },
+  {
+    "revision": "9866c579b5c9c66208c7",
+    "url": "/Robert-OP/contact-manager/static/css/main.f5741332.chunk.css"
+  },
+  {
+    "revision": "5e73e9b3d7a43029ad7b",
+    "url": "/Robert-OP/contact-manager/static/css/1.ecb123a8.chunk.css"
+  },
+  {
+    "revision": "0ba0181337c2a569868c903ddda7d839",
+    "url": "/Robert-OP/contact-manager/index.html"
+  }
+];
