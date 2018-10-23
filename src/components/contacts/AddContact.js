b7105b10c7ab156e0f724a3d742b557d55f4bbb1
Robert-OP <popescu.robert.octavian@gmail.com>
2018-10-23 17:37:41 +0200

    contact manager react react-router context-api

---
+++ src/components/contacts/AddContact.js
@@ -0,0 +1,116 @@
+import React, { Component } from 'react';
+import { Consumer } from '../../context';
+import uuid from 'uuid';
+import TextInput from '../layout/TextInput';
+import axios from 'axios';
+
+class AddContact extends Component {
+  state = {
+    name: '',
+    email: '',
+    phone: '',
+    errors: {}
+  };
+
+  onSubmit = async (dispatch, e) => {
+    e.preventDefault();
+
+    const { name, email, phone } = this.state;
+
+    // check for errors
+    if (name === '') {
+      this.setState({ errors: { name: 'Name is required' } });
+      return;
+    }
+
+    if (email === '') {
+      this.setState({ errors: { email: 'Email is required' } });
+      return;
+    }
+
+    if (phone === '') {
+      this.setState({ errors: { phone: 'Phone is required' } });
+      return;
+    }
+
+    const newContact = {
+      id: uuid(),
+      name,
+      email,
+      phone
+    };
+
+    const res = await axios.post(
+      'https://jsonplaceholder.typicode.com/users',
+      newContact
+    );
+
+    dispatch({ type: 'ADD_CONTACT', payload: res.data });
+
+    // clear state
+    this.setState({
+      name: '',
+      email: '',
+      phone: '',
+      errors: {}
+    });
+
+    // redirect
+    this.props.history.push('/');
+  };
+
+  onChange = e => this.setState({ [e.target.name]: e.target.value });
+
+  render() {
+    const { name, email, phone, errors } = this.state;
+
+    return (
+      <Consumer>
+        {value => {
+          const { dispatch } = value;
+          return (
+            <div className="card mb-3">
+              <div className="card-header">Add Contact</div>
+              <div className="card-body">
+                <form onSubmit={this.onSubmit.bind(this, dispatch)}>
+                  <TextInput
+                    name="name"
+                    label="Name"
+                    placeholder="Enter name..."
+                    value={name}
+                    onChange={this.onChange}
+                    error={errors.name}
+                  />
+                  <TextInput
+                    name="email"
+                    label="Email"
+                    placeholder="Enter email..."
+                    type="emai"
+                    value={email}
+                    onChange={this.onChange}
+                    error={errors.email}
+                  />
+                  <TextInput
+                    name="phone"
+                    label="Phone"
+                    placeholder="Enter phone..."
+                    value={phone}
+                    onChange={this.onChange}
+                    error={errors.phone}
+                  />
+                  <input
+                    type="submit"
+                    value="Add Contact"
+                    className="btn btn-light btn-block"
+                  />
+                </form>
+              </div>
+            </div>
+          );
+        }}
+      </Consumer>
+    );
+  }
+}
+
+export default AddContact;
